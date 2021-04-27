2585749c4808523af1ccb099a1b9120ff6e8b603
Elisenda Monserrat <elisenda.monserrat@gmail.com>
2021-04-27 11:13:37 +0200

    adds httpshttp-errors npm package

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+const createError = require('http-errors');
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const favicon = require('serve-favicon');
 const hbs = require('hbs');
 const mongoose = require('mongoose');
@@ -21,8 +21,8 @@ require('./configs/db.config');
 
 // Middleware Setup
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Express View engine setup
@@ -40,5 +40,22 @@ app.use('/', index);
 
 const droneRoutes = require('./routes/drones');
 app.use('/', droneRoutes);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
